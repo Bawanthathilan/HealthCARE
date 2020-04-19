7543542d94a0eb65409b9d89abc99cd80a16dfa9
Savandi <savandi98@gmail.com>
2020-04-19 18:37:02 +0530

    DB name changed to Doctor from paf

--- DoctorManagement/src/main/java/com/doctor/utils/DoctorDBConnection.java
+++ DoctorManagement/src/main/java/com/doctor/utils/DoctorDBConnection.java
@@ -17,10 +17,10 @@ public class DoctorDBConnection {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf", "root", "");
-        } catch (ClassNotFoundException e) {
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Doctor", "root", "");
+        } catch (ClassNotFoundException e){
             System.out.println("Driver Class Not Found");
-        } catch (SQLException e) {
+        }catch (SQLException e){
             createDatabase();
             createTable();
         }
@@ -35,10 +35,11 @@ public class DoctorDBConnection {
     private static void createDatabase() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306","root","");
             Statement s = con.createStatement();
-            s.executeUpdate("CREATE DATABASE IF NOT EXISTS paf");
-        } catch (ClassNotFoundException | SQLException e) {
+            s.executeUpdate("CREATE DATABASE IF NOT EXISTS Doctor");
+        }
+        catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
@@ -47,14 +48,16 @@ public class DoctorDBConnection {
         String myTableName = "create table regDoctors(doctor_id INT, firstName varchar(40),lastName varchar(50),gender varchar(10),email varchar(50),password varchar(50),joinedDate varchar(30),phone INT,specialization varchar(40),address varchar(50),NIC varchar(10),hospital_id INT)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Doctor", "root", "");
             Statement s = con.createStatement();
             //The next line has the issue
             s.executeUpdate(myTableName);
             System.out.println("Table Created");
-        } catch (SQLException e) {
+        }
+        catch (SQLException e ) {
             System.out.println("An error has occurred on Table Creation");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e) {
             System.out.println("An Mysql drivers were not found");
         }
     }
